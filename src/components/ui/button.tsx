18862c1eac09cd0b5d0825b7c9209799c67324e6
Renato-Rodrigues83 <renatinprado8832@gmail.com>
2026-04-29 18:21:55 -0300

    add Markdown and Share component's

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,10 +9,12 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        primary: "bg-blue-200 text-white hover:bg-blue-100 rounded-full",
-        secondary: "bg-gray-100 text-gray-800 hover:bg-blue-100 rounded-full",
+        primary:
+          "bg-blue-200 text-white hover:bg-blue-100 rounded-full cursor-pointer",
+        secondary:
+          "bg-gray-100 text-gray-800 hover:bg-blue-100 rounded-full cursor-pointer",
         outline:
-          "border border-gray-400 bg-gray-700 transition-colors duration-200 hover:text-blue-200 hover:border-blue-200",
+          "border border-gray-400 bg-gray-700 transition-colors duration-200 hover:text-blue-200 hover:border-blue-200 cursor-pointer",
       },
       size: {
         default: "h-10 px-4 py-2",
--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,4 +1,5 @@
 import { Avatar } from "@/components/avatar";
+import { Markdown } from "@/components/markdown";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -6,6 +7,7 @@ import {
   BreadcrumbList,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
 import { allPosts } from "contentlayer/generated";
 import Image from "next/image";
 import Link from "next/link";
@@ -22,51 +24,74 @@ export default function PostPage() {
 
   return (
     <main className="mt-32 text-gray-100">
-      <Breadcrumb>
-        <BreadcrumbList>
-          <BreadcrumbItem>
-            <BreadcrumbLink asChild className="text-action-sm">
-              <Link href="/blog">Blog</Link>
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <span className="text-blue-200 text-action-sm">{post?.title}</span>
-          </BreadcrumbItem>
-        </BreadcrumbList>
-      </Breadcrumb>
+      <div className="container space-y-12 px-4 md:px-8">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink asChild className="text-action-sm">
+                <Link href="/blog">Blog</Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <span className="text-blue-200 text-action-sm">
+                {post?.title}
+              </span>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
 
-      <div className="grid grid-cols-1 lg:grid-cols-[1fr_300px] gap-6 lg:gap-12">
-        <article className="bg-gray-600 rounded-lg overflow-hidden border-gray-400 border">
-          <figure className="relative aspect-16/10 w-full overflow-hidden rounded-lg">
-            <Image
-              src={post?.image ?? ""}
-              alt={post?.title ?? ""}
-              fill
-              className="object-cover"
-            />
-          </figure>
-          <header className="p-4 md:p-6 lg:p-12 pb-0">
-            <h1 className="mb-6 text-balance text-heading-lg md:text-heading-xl">
-              {post?.title}
-            </h1>
-
-            <Avatar.Container>
-              <Avatar.Image
-                src={post?.author.avatar.trimEnd()}
-                alt={post?.title.trimEnd()}
-                size="sm"
+        <div className="grid grid-cols-1 lg:grid-cols-[1fr_300px] gap-6 lg:gap-12">
+          <article className="bg-gray-600 rounded-lg overflow-hidden border-gray-400 border">
+            <figure className="relative aspect-16/10 w-full overflow-hidden rounded-lg">
+              <Image
+                src={post?.image ?? ""}
+                alt={post?.title ?? ""}
+                fill
+                className="object-cover"
               />
-              <Avatar.Content>
-                <Avatar.Title>{post?.author.name}</Avatar.Title>
-                <Avatar.Description>
-                  Publicado em{" "}
-                  <time dateTime={post?.date}>{publishedDate}</time>
-                </Avatar.Description>
-              </Avatar.Content>
-            </Avatar.Container>
-          </header>
-        </article>
+            </figure>
+            <header className="p-4 mt-8 md:mt-12 md:p-6 lg:p-12 pb-0">
+              <h1 className="mb-8 text-balance text-heading-lg md:text-heading-xl">
+                {post?.title}
+              </h1>
+
+              <Avatar.Container>
+                <Avatar.Image
+                  src={post?.author.avatar.trimEnd()}
+                  alt={post?.title.trimEnd()}
+                  size="sm"
+                />
+                <Avatar.Content>
+                  <Avatar.Title>{post?.author.name}</Avatar.Title>
+                  <Avatar.Description>
+                    Publicado em{" "}
+                    <time dateTime={post?.date}>{publishedDate}</time>
+                  </Avatar.Description>
+                </Avatar.Content>
+              </Avatar.Container>
+            </header>
+
+            <div className="prose prove-invert maxmax-w-none px-4 mt-12 md:px-6 lg:px-12">
+              <Markdown content={post.body.raw} />
+            </div>
+          </article>
+
+          <aside className="space-y-6">
+            <div className="rounded-lg bg-gray-700 p-4 md:p-6">
+              <h2 className="mb-4 text-heading-xs text-gray-100">
+                Compartilhar
+              </h2>
+              <div className="space-y-6">
+                {[{ key: "1", providerName: "LinkedIn" }].map((provider) => (
+                  <Button key={provider.key} variant="outline">
+                    {provider.providerName}
+                  </Button>
+                ))}
+              </div>
+            </div>
+          </aside>
+        </div>
       </div>
     </main>
   );

374325db6181691f992167aee04704d5d13581cb
Renato-Rodrigues83 <renatinprado8832@gmail.com>
2026-05-07 15:32:06 -0300

    Add SSG for Post pages

--- src/hooks/use-share/use-share.tsx
+++ src/hooks/use-share/use-share.tsx
@@ -5,7 +5,7 @@ import {
   SocialProvider,
 } from "./social-providers";
 import { useClipboard } from "../use-clipboard";
-import { Link2 } from "lucide-react";
+import { Link } from "lucide-react";
 
 type useShareProps = ShareConfig & { clipboardTimeout?: number };
 
@@ -64,7 +64,7 @@ export const useShare = ({
       {
         provider: "clipboard",
         name: isCopied ? "Link copiado" : "Copiar link",
-        icon: <Link2 className="h-4 w-4" />,
+        icon: <Link className="h-4 w-4" />,
         action: () => share("clipboard"),
       },
     ],
--- src/templates/blog/blog-list.tsx
+++ src/templates/blog/blog-list.tsx
@@ -2,10 +2,14 @@ import { Search } from "@/components/search";
 import { useRouter } from "next/router";
 import { PostCard } from "./components/post-card";
 import { PostGridCard } from "./components/post-grid-card";
-import { allPosts } from "contentlayer/generated";
+import { Post } from "contentlayer/generated";
 import { Inbox } from "lucide-react";
 
-export function BlogList() {
+export type BlogListProps = {
+  posts: Post[];
+};
+
+export function BlogList({ posts }: BlogListProps) {
   const router = useRouter();
   const query = router.query.q as string;
 
@@ -13,12 +17,12 @@ export function BlogList() {
     ? `Resultados de busca para "${query}"`
     : "Dicas e estratégias para impulsionar seu negócio";
 
-  const posts = query
-    ? allPosts.filter((post) =>
+  const postList = query
+    ? posts.filter((post) =>
         post.title.toLowerCase()?.includes(query.toLowerCase()),
       )
-    : allPosts;
-  const hasPosts = posts.length > 0;
+    : posts;
+  const hasPosts = postList.length > 0;
 
   return (
     <div className="flex flex-col py-24 grow h-full">

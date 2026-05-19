0466963ed833fd57863f23ee523974281a55103e
Renato-Rodrigues83 <renatinprado8832@gmail.com>
2026-05-19 16:14:54 -0300

    deleting post-page on pages router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,19 @@
 import { LandingPage } from "@/templates/landing-page/landing-page";
+/*import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Site.set",
+  description: "Venda seus produtos como afiliado em um único lugar",
+  robots: "index, follow",
+  openGraph: {
+    title: "Site.set",
+    description: "Venda seus produtos como afiliado em um único lugar",
+    url: "https://site.set",
+    siteName: "Site.set",
+    type: "website",
+
+  },
+  };*/
 
 export default function HomePage() {
   return <LandingPage />;
